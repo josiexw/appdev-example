b512d6a3471271efb379c22ba7bb499ef67ca696
Josephine Wang <josiexw@mit.edu>
2025-04-25 18:42:55 -0400

    cors

--- server/server.py
+++ server/server.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, supports_credentials=True)
 
 # Create database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
